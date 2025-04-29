dda378f612d97ec113df768908f95a11f5bc7ad0
Aashutosh kumar bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-04-29 22:29:11 +0530

    Updated: short description of what changed

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,28 @@ app.use(express.json());
 
 
 app.post('/todo', function (req, res) {
-
+    const createpayload = req.body;
+    const parsedPayload = createtodo.safeparse(createpayload);
+    if (!parsedPayload.success) {
+        return res.status(400).json({
+            msg: 'Invalid payload',
+        });
+        return;
+    }
+  //put in mongoose
 });
 
 app.get('/todos', function (req, res) {
-
+        
 });
 
 app.put('/completed', function (req, res) {
-
+        const updatepayload = req.body;
+        const parsedPayload = updatetodo.safeparse(updatepayload);
+        if (!parsedPayload.success) {
+            return res.status(400).json({
+                msg: 'Invalid payload',
+            });
+            return;
+        }
 });
