6b2470ac2e4e32754338bed72815f019b0b93cc4
Aashutosh kumar bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-04-29 22:38:38 +0530

    added

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { string } = require('zod');
+const { Schema } = mongoose;
+
+mongoose.connect("mongodb+srv://aashutosh:aashu@aashutoshkr.uceqeb2.mongodb.net/todo-app")
+const todoschema = mongoose.Schema({
+    title:string,
+    description:string,
+    completed:boolean,
+})
+
+const todo = mongoose.model('todo', todoschema);
+
+module.export ={
+    todo
+}
