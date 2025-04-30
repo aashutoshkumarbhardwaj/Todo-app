1c0f2a7968f913dba9d38df673af0f11206e1969
Aashutosh kumar bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2025-04-30 18:52:19 +0530

    working day

---
+++ frontend/src/component/createtodo.jsx
@@ -0,0 +1,9 @@
+export function createtodo(){
+    return <div>
+        <input type="text" placeholder="title" />
+        <input type="text" placeholder="description" />
+
+        <button >Add a todo </button>
+        </div>
+}
+
